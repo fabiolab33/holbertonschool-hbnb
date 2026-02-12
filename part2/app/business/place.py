39e6449bb8e5b6e31ab3e55bc86eaf10bc39d41a
Fabiola Burgos <fburgos913@gmail.com>
2026-02-12 11:54:12 -0600

    Run

--- part2/app/business/place.py
+++ part2/app/business/place.py
@@ -1,16 +1,15 @@
-# app/business/place.py
 from app.business.base_model import BaseModel
 
 class Place(BaseModel):
-        """Place entity representing a property listing"""
-        
-        def __init__(self, title, description, price, latitude, longitude, owner_id):
+    """Place entity representing a property listing"""
+
+    def __init__(self, title, description, price, latitude, longitude, owner_id):
         super().__init__()
         self.title = title
         self.description = description
         self.price = price
         self.latitude = latitude
         self.longitude = longitude
-        self.owner_id = owner_id  # Task 1 reference to User.id
-        self.reviews = []         # Task 1 list of Review
-        self.amenities = []       # Task 1 list of Amenity
+        self.owner_id = owner_id  # reference to User.id
+        self.reviews = []         # list of Review
+        self.amenities = []       # list of Amenity
