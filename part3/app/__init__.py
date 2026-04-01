5a7496762fa45d72a42f0622e159777b33d61197
Fabiola Burgos <fburgos913@gmail.com>
2026-03-31 23:03:04 -0700

    Add cors

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -4,6 +4,7 @@ from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -25,6 +26,9 @@ def create_app(config_name=None):
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+
+    # Enable CORS
+    CORS(app)
 
     # Initialize API
     api = Api(
